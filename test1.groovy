7e580bc08bc5af43781c38c37c96822257142f57
tony zhao <tony_zyt@hotmail.com>
2015-11-08 18:34:50 +0100

    added test1.groovy

---
+++ test1.groovy
@@ -0,0 +1,11 @@
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('git://github.com/jgritman/aws-sdk-test.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
